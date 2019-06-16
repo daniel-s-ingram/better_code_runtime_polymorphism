76e1b6e6d6ae63af173e32914e3dff2ac9e6fdec
Daniel Ingram <ingramds@appstate.edu>
2019-06-16 11:34:01 -0400

    Fourth iteration, added copy constructor and copy assignment operator

--- library.hpp
+++ library.hpp
@@ -14,15 +14,36 @@ void draw(const int& x, std::ostream& out, size_t position)
 class object_t
 {
 public:
-    object_t(const int& x) : self_(x) {}
+    object_t(const int& x) : self_(std::make_unique<int_model_t>(x)) {}
+
+    object_t(const object_t& x) : self_(std::make_unique<int_model_t>(*x.self_)) {}
     
+    object_t& operator=(const object_t& x)
+    {
+        object_t tmp(x);
+        self_ = std::move(tmp.self_);
+        return *this;
+    }
+
     friend void draw(const object_t& x, std::ostream& out, size_t position)
     {
-        draw(x.self_, out, position);
+        x.self_->draw_(out, position);
     }
 
 private:
-    int self_;
+    struct int_model_t
+    {
+        int_model_t(const int& x) : data_(x) {}
+
+        void draw_(std::ostream& out, size_t position) const
+        {
+            draw(data_, out, position);
+        }
+
+        int data_;
+    };
+
+    std::unique_ptr<int_model_t> self_;
 };
 
 using document_t = std::vector<object_t>;
